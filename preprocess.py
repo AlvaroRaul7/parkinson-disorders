c363dd0055146180e410fa5951c5522970e34768
Alvaro Valarezo <lfvalare@espol.edu.ec>
2021-05-31 13:01:25 -0400

    denoising preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,14 @@
 
-#Author: Alvaro Valarezo 
-import dicom2nifti
+#Author: Alvaro Valarezo
 
+import dicom2nifti
+import ants
+
+#Declaration of variables
+
+example_path='./niidata/6_sag_3d_fspgr_bravo_straight.nii.gz'
+
+denoise_path='./denoisedata/image.nii.gz'
 
 # Function to convert DICOM files to NII format in order to pre-process them.
 def dicom2nii():
@@ -9,4 +16,24 @@ def dicom2nii():
 
     output_folder = './niidata/'
     dicom2nifti.convert_directory(dicom_directory, output_folder)  
+
+# Function to get nii orientation
+def get_orientation(path):
+    z = ants.image_read(path)
+    original_ori = z.get_orientation() # eg: 'RPI','LPI','RAI',etc 
+    print(original_ori)
+
+# Function to get image header of nii file
+def get_header(path):
+    image_header = ants.image_header_info(example_path)
+    print(image_header)
+
+# Function to denoise image using ANTs
+def denoise_image(path):
+    img= ants.image_read(path) # Read nii image and convert it to an ANTs array
+    denoise= ants.denoise_image(img, noise_model='gaussian') 
+    ants.image_write(denoise,denoise_path) #Write image in an specific path 
+
+
+
 
