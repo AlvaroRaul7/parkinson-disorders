6a971920cfde9a3918738c82befabc574cc47953
Alvaro Valarezo <lfvalare@espol.edu.ec>
2021-05-31 14:33:12 -0400

    bias field correction

--- preprocess.py
+++ preprocess.py
@@ -10,6 +10,8 @@ example_path='./niidata/6_sag_3d_fspgr_bravo_straight.nii.gz'
 
 denoise_path='./denoisedata/image.nii.gz'
 
+bias_correction_path='./biasdata/image.nii.gz'
+
 # Function to convert DICOM files to NII format in order to pre-process them.
 def dicom2nii():
     dicom_directory= './data/'
@@ -20,7 +22,7 @@ def dicom2nii():
 # Function to get nii orientation
 def get_orientation(path):
     z = ants.image_read(path)
-    original_ori = z.get_orientation() # eg: 'RPI','LPI','RAI',etc 
+    original_ori = z.get_orientation() # eg: The MRis are in a RPI orientation
     print(original_ori)
 
 # Function to get image header of nii file
@@ -32,9 +34,12 @@ def get_header(path):
 def denoise_image(path):
     img= ants.image_read(path) # Read nii image and convert it to an ANTs array
     denoise= ants.denoise_image(img, noise_model='gaussian') 
-    ants.image_write(denoise,denoise_path) #Write image in an specific path 
+    ants.image_write(denoise,denoise_path) #Write image in a specific path 
 
-
+def bias_field_correction(path):
+    img= ants.image_read(path)
+    bias_correction = ants.n4_bias_field_correction(img)
+    ants.image_write(bias_correction, bias_correction_path)
 
 
 
